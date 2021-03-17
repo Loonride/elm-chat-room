7742bafd7e244281d7e9a8866d132bd8e97ecd1d
Loonride <22580625+Loonride@users.noreply.github.com>
2021-03-17 01:06:12 -0500

    finishing touches

--- server/www/handler.js
+++ server/www/handler.js
@@ -2,7 +2,7 @@ const app = Elm.Client.Client.init({
     node: document.getElementById('elm'),
 });
 
-const ws = new WebSocket('ws://localhost:8000');
+const ws = new WebSocket(`ws://${window.location.host}`);
 
 ws.onmessage = (msg) => {
     app.ports.inputPort.send(msg.data);
